635c22aab44402dbaba14e53551742f47847b48f
bchavdarov <spo@abv.bg>
2020-05-09 18:56:31 +0300

    header change

--- header.php
+++ header.php
@@ -23,46 +23,48 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bcdl-usbs-01' ); ?></a>
+	<div id="wrapper-navbar">
+		<a class="skip-link sr-only sr-only-focusable skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bcdl-usbs-01' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$bcdl_usbs_01_description = get_bloginfo( 'description', 'display' );
+				if ( $bcdl_usbs_01_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $bcdl_usbs_01_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+			 <!--nav site navigation-->
+				<button class="navbar-toggler menu-toggle" type="button" data-toggle="collapse" data-target="#bcdl-navbar-dropdown01" aria-controls="bcdl-navbar-dropdown01" aria-expanded="false" aria-label="<?php esc_html_e( 'Primary Menu', 'bcdl-usbs-01' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
 				<?php
-			endif;
-			$bcdl_usbs_01_description = get_bloginfo( 'description', 'display' );
-			if ( $bcdl_usbs_01_description || is_customize_preview() ) :
+				wp_nav_menu( array(
+	            'theme_location'    => 'primary',
+	            'depth'             => 2,
+	            'container'         => 'div',
+	            'container_class'   => 'collapse navbar-collapse',
+	            'container_id'      => 'bcdl-navbar-dropdown01',
+	            'menu_class'        => 'nav navbar-nav',
+	            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+	            'walker'            => new BCDL_WP_Bootstrap_Navwalker(),
+	        ) );
+	        ?>
 				?>
-				<p class="site-description"><?php echo $bcdl_usbs_01_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		 <!--nav site navigation-->
-			<button class="navbar-toggler menu-toggle" type="button" data-toggle="collapse" data-target="#bcdl-navbar-dropdown01" aria-controls="bcdl-navbar-dropdown01" aria-expanded="false" aria-label="<?php esc_html_e( 'Primary Menu', 'bcdl-usbs-01' ); ?>">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<?php
-			wp_nav_menu( array(
-            'theme_location'    => 'primary',
-            'depth'             => 2,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bcdl-navbar-dropdown01',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new BCDL_WP_Bootstrap_Navwalker(),
-        ) );
-        ?>
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+	</div>

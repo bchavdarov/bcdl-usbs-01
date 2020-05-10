ee5831b23c46af1fc35332d40ba2004c90d52801
bchavdarov <spo@abv.bg>
2020-05-10 10:27:56 +0300

    php bug fix

--- functions.php
+++ functions.php
@@ -157,9 +157,6 @@ function bcdl_usbs_01_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'bcdl_usbs_01_scripts' );
 
-wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bcdlcss/theme.css' );
-wp_enqueue_script( 'bootstrap-js' , get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array('jquery') );
-
 /**
  * Implement the Custom Header feature.
  */
@@ -202,3 +199,5 @@ function register_navwalker(){
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
 
+wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bcdlcss/theme.css' );
+wp_enqueue_script( 'bootstrap-js' , get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array('jquery') );
